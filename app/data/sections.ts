a5ca9b6cdd06c3064f1a65fc6e3f58d101f2ce6f
Bilal Salaymah <bilal@MacBook-Air-van-Bilal.local>
2025-05-31 00:18:45 +0200

    Mijn updates

--- app/data/sections.ts
+++ app/data/sections.ts
@@ -1,10 +1,37 @@
 export const sections = [
-    {
-      slug: "faq-1",
-      title: "FAQ 1",
-      price: "$9",
-      image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
-      description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
-    },
-    // Je kunt hier makkelijk meer toevoegen
-  ];
+  {
+    slug: "faq-1",
+    title: "FAQ 1",
+    price: "$9",
+    image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
+    description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
+  },
+  {
+    slug: "faq-2",
+    title: "FAQ 2",
+    price: "$9",
+    image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
+    description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
+  },
+  {
+    slug: "faq-3",
+    title: "FAQ 3",
+    price: "$9",
+    image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
+    description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
+  },
+  {
+    slug: "faq-4",
+    title: "FAQ 4",
+    price: "$9",
+    image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
+    description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
+  },
+  {
+    slug: "faq-5",
+    title: "FAQ 5",
+    price: "$9",
+    image: "https://d1xgiem9kow01r.cloudfront.net/4tamd2gafh7tzlr8iyalmgz8rfbf",
+    description: "A simple and professional FAQ section layout to build trust and answer customer questions.",
+  },
+];
